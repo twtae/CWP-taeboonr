2c83c96a997e379848c1266bf64992c5e4361e99
Taewich Boonrerm <taewich01@outlook.com>
2026-09-24 00:34:30 +0700

    Change output follow the instruction

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -3,5 +3,5 @@
 array = [1, 2, 6, 7, 67, 76, 2, 1]
 new_array = [i + 2 for i in array if i > 5]
 
-print(f"Original array: {array}")
-print(f"New array: {new_array}")
+print(array)
+print(new_array)
