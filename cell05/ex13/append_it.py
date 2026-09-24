9653415d3e6529e0bd87ac18980d631158977d49
Taewich Boonrerm <taewich01@outlook.com>
2026-09-24 09:48:22 +0700

    Add cell05/ex13

---
+++ cell05/ex13/append_it.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import sys
+
+if (len(sys.argv) == 1):
+    print("none")
+else:
+    for param in sys.argv[1:]:
+        if not param.endswith("ism"):
+            print(f"{param}ism")
